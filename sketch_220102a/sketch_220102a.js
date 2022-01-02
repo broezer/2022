95c058f30cd6e0629b7d5987c6e78fb9c010510f
broezer <bruce@moerdjiman.com>
2022-01-02 12:05:23 +0100

    DITHERING

---
+++ sketch_220102a/sketch_220102a.js
@@ -0,0 +1,133 @@
+const Y_AXIS = 1;
+let c1, c2;
+let img;
+
+function setup() {
+  createCanvas(1000, 1000,);
+  img = createGraphics(1000,4000);
+  
+  c1 = color(255, 179, 13);
+  c2 = color(200,64,200);
+  
+  setGradient(0, 0, width, height, c2, c1, Y_AXIS); 
+  
+  for (var i=0; i<10; i+=1){
+    for (var j=0; j<10; j+=1){
+      setGradient( (width/40) + (width/10 * i), (height/40) + (height/10 * j), width/20, height/20, c1, c2, Y_AXIS); 
+    } 
+  }
+  
+  image(img, 0, 0);
+  makeDithered(img, 1.01);
+  image(img, 0, 0)
+  
+  save("20220102.png");
+}
+
+
+function draw() {
+   
+
+
+}
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+
+  if (axis === Y_AXIS) {
+    // Top to bottom gradient
+    for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      img.stroke(c);
+      img.line(x, i, x + w, i);
+    }
+  } else if (axis === X_AXIS) {
+    // Left to right gradient
+    for (let i = x; i <= x + w; i++) {
+      let inter = map(i, x, x + w, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      img.stroke(c);
+      img.line(i, y, i, y + h);
+    }
+  }
+}
+
+
+function imageIndex(img, x, y) {
+  return 4 * (x + y * img.width);
+}
+
+function getColorAtindex(img, x, y) {
+  let idx = imageIndex(img, x, y);
+  let pix = img.pixels;
+  let red = pix[idx];
+  let green = pix[idx + 1];
+  let blue = pix[idx + 2];
+  let alpha = pix[idx + 3];
+  return color(red, green, blue, alpha);
+}
+
+function setColorAtIndex(img, x, y, clr) {
+  let idx = imageIndex(img, x, y);
+
+  let pix = img.pixels;
+  pix[idx] = red(clr);
+  pix[idx + 1] = green(clr);
+  pix[idx + 2] = blue(clr);
+  pix[idx + 3] = alpha(clr);
+}
+
+// Finds the closest step for a given value
+// The step 0 is always included, so the number of steps
+// is actually steps + 1
+function closestStep(max, steps, value) {
+  return round(steps * value / 255) * floor(255 / steps);
+}
+
+function makeDithered(img, steps) {
+  img.loadPixels();
+
+  for (let y = 0; y < img.height; y++) {
+    for (let x = 0; x < img.width; x++) {
+      let clr = getColorAtindex(img, x, y);
+      let oldR = red(clr);
+      let oldG = green(clr);
+      let oldB = blue(clr);
+      let newR = closestStep(255, steps, oldR);
+      let newG = closestStep(255, steps, oldG);
+      let newB = closestStep(255, steps, oldB);
+
+      let newClr = color(newR, newG, newB);
+      setColorAtIndex(img, x, y, newClr);
+
+      let errR = oldR - newR;
+      let errG = oldG - newG;
+      let errB = oldB - newB;
+
+      distributeError(img, x, y, errR, errG, errB);
+    }
+  }
+
+  img.updatePixels();
+}
+
+function distributeError(img, x, y, errR, errG, errB) {
+  addError(img, 7 / 16.0, x + 1, y, errR, errG, errB);
+  addError(img, 3 / 16.0, x - 1, y + 1, errR, errG, errB);
+  addError(img, 5 / 16.0, x, y + 1, errR, errG, errB);
+  addError(img, 1 / 16.0, x + 1, y + 1, errR, errG, errB);
+}
+
+function addError(img, factor, x, y, errR, errG, errB) {
+  if (x < 0 || x >= img.width || y < 0 || y >= img.height) return;
+  let clr = getColorAtindex(img, x, y);
+  let r = red(clr);
+  let g = green(clr);
+  let b = blue(clr);
+  clr.setRed(r + errR * factor);
+  clr.setGreen(g + errG * factor);
+  clr.setBlue(b + errB * factor);
+
+  setColorAtIndex(img, x, y, clr);
+}
