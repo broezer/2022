78555b91e08cf04eae90f4e70d5c1872453e5856
broezer <bruce@moerdjiman.com>
2022-01-10 19:05:04 +0100

    ML_GONE_WRONG_SOL_LEWIT_WALL_DRAWING_413

--- sketch_220110a/sketch_220110a.js
+++ sketch_220110a/sketch_220110a.js
@@ -26,65 +26,63 @@ function fourByfour(x,y, width, height){
   noStroke();
   textSize(52);
   textAlign(CENTER);
-  //const firstColor = colors[random];
-  const random = Math.floor(Math.random() * colors.length);
-
-  let index01 = colors.indexOf(colors[random]);
-  let color01 = colorNames[index01];
-  let colorIndex01 = colorNames.indexOf(color01);
   
-  let firstColor = colors[random];
-  fill(colors[random]);
-  rect(x, y, width/2, height/2)
-  
+  random = Math.floor(Math.random() * colors.length);
+  color01 = colors[random];
+  index01 = colors.indexOf(colors[random]);
+  color01Name = colorNames[index01];
   
   if (index01 > -1) {
       colors.splice(index01, 1);
-      colorNames.splice(colorIndex01,1);
+      colorNames.splice(index01, 1);
       
   }
   
-  const random02 = Math.floor(Math.random() * colors.length);
-  
-  fill(colors[random02]);
-  text(color01, x + width/4, y + height/4 + 12)
-  
-  let index02 = colors.indexOf(colors[random02]);
-  let color02 = colorNames[index02];
-  let colorIndex02 = colorNames.indexOf(color02);
-  
-  
-  rect( x + width/2, y, width/2, height/2)
-
+  random02 = Math.floor(Math.random() * colors.length);
+  color02 = colors[random02];
+  index02 = colors.indexOf(colors[random02]);
+  color02Name = colorNames[index02];
   if (index02 > -1) {
       colors.splice(index02, 1);
-     colorNames.splice(colorIndex02,1);
+      colorNames.splice(index02, 1);
   }
   
-  const random03 = Math.floor(Math.random() * colors.length);
-  
-  
-  let index03 = colors.indexOf(colors[random03]);
-  let color03 = colorNames[index03];
-  let colorIndex03 = colorNames.indexOf(color03);
-  
-  fill(colors[random03]);
-  rect(x + width/2,  y + height/2, width/2, height/2);
-  text(color02, x + width/2 + width/4, y + height/4 + 12)
-
-  
-  
+  random03 = Math.floor(Math.random() * colors.length);
+  color03 = colors[random03];
+  index03 = colors.indexOf(colors[random03]);
+  color03Name = colorNames[index03];
+ 
   if (index03 > -1) {
       colors.splice(index03, 1);
-      colorNames.splice(colorIndex03,1);
+      colorNames.splice(index03, 1);
   }
-  fill(colors[0]);
+  
+  color04 = colors[0]
+  color04Name = colorNames[0];
+
+
+  fill(color03);
+  rect(x + width/2,  y + height/2, width/2, height/2);
+
+  fill(color01);
+  rect(x, y, width/2, height/2)
+  
+  fill(color04);
   rect(x, y + height/2, width/2, height/2);
-  text(color03, x + width/2 + width/4, y + height/2 + height/4 + 12)
   
-  fill(firstColor);
+  fill(color02);
+  rect( x + width/2, y, width/2, height/2)
   
-  text(colorNames[0], x + width/4, y + height/2 + height/4 + 12)
+  text(color01Name, x + width/2 + width/4, y + height/2 + height/4 + 12);
+  
+  fill(color03);
+  text(color02Name, x + width/4, y + height/2 + height/4 + 12);
+  
+  fill(color04);
+  text(color03Name, x + width/4, y + height/4 + 12);
+  
+  fill(color01);
+  text(color04Name, x + width/2 + width/4, y + height/4 + 12);
 
   
 }
